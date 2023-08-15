99e61a845ae04fcc16630bba87dde2eae1625657
Varsha Poswalia <97456495+vposwalia@users.noreply.github.com>
2023-08-15 17:12:49 +0530

    changes made

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
-const path = require('path'); // Import the 'path' module
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 4002;
@@ -21,24 +21,39 @@ connection.connect((error) => {
     console.log('Connected to database');
 });
 
-// Configure EJS as the view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-// Serve static files from the public directory
-app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Define a route for the root URL
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.render('index.ejs'); // Render the EJS template
+    res.render('index.ejs');
+});
+
+app.get('/results', (req, res) => {
+    const sortField = req.query.sort || 'id';
+    const sortOrder = req.query.order || 'asc';
+    
+    let query = 'SELECT * FROM missions ORDER BY ?? ' + sortOrder;
+    const inserts = [sortField];
+
+    query = mysql.format(query, inserts);
+
+    connection.query(query, (error, results) => {
+        if (error) {
+            console.error('Error fetching data:', error);
+            return res.status(500).send('An error occurred');
+        }
+        res.render('result.ejs', { results, sortField, sortOrder });
+    });
 });
 
 app.post('/submit', (req, res) => {
     const formData = req.body;
-
     const query = 'INSERT INTO missions SET ?';
     connection.query(query, formData, (error, results) => {
         if (error) {
@@ -46,6 +61,17 @@ app.post('/submit', (req, res) => {
             return res.status(500).send('An error occurred');
         }
         res.sendStatus(200);
+    });
+});
+app.post('/delete/:id', (req, res) => {
+    const missionId = req.params.id;
+    const query = 'DELETE FROM missions WHERE id = ?';
+    connection.query(query, [missionId], (error, results) => {
+        if (error) {
+            console.error('Error deleting data:', error);
+            return res.status(500).send('An error occurred');
+        }
+        res.redirect('/results'); // Redirect to the results page after deletion
     });
 });
 
